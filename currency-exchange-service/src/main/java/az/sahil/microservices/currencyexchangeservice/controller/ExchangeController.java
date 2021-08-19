0f3e46800c8812c42934e4c345a21cebcdb8e73e
AppayevS <appayevs@kapitalho.az>
2021-08-19 23:10:11 +0400

    Added zipkin distributed tracing system and port&urls file

--- currency-exchange-service/src/main/java/az/sahil/microservices/currencyexchangeservice/controller/ExchangeController.java
+++ currency-exchange-service/src/main/java/az/sahil/microservices/currencyexchangeservice/controller/ExchangeController.java
@@ -3,12 +3,14 @@ package az.sahil.microservices.currencyexchangeservice.controller;
 import az.sahil.microservices.currencyexchangeservice.entity.CurrencyExchange;
 import az.sahil.microservices.currencyexchangeservice.repository.CurrencyExchangeRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RequiredArgsConstructor
 @RestController
 @RequestMapping("currency-exchange")
@@ -26,6 +28,7 @@ public class ExchangeController {
         CurrencyExchange currencyExchange = exchangeRepository.findByFromAndTo(from.toUpperCase(), to.toUpperCase())
                 .orElseThrow(() -> new RuntimeException(CurrencyExchange.class.getName() + " not found!"));
         currencyExchange.setEnvironment("Port: " + port + " Env: " + env);
+        log.info("getCurrencyExchange called with from {} to {}", from, to);
         return currencyExchange;
     }
 
